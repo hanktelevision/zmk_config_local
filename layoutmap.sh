5dc476787768be597e40c1d9ef63ac5df38d00ad
hanktelevision <hanktelevision@gmail.com>
2024-11-08 16:58:31 -0500

    check small changes

--- layoutmap.sh
+++ layoutmap.sh
@@ -4,9 +4,13 @@ keymap='./config/corne.keymap'
 $(cat "$keymap" | sed -ne '32,39p;51,58p;70,77p'> "$_temp")
 #$(cat "$_temp" | sed -e 's,^..,||,g; s,^.\{30\}$,&--,g; s/\(.\{73\}\)/\1||/g' > ./keymapPrint.txt)
 #$(cat ./test2.txt | awk '{"*" $0 "*"}')>>./test2.txt
+$(cat temp.txt | sed -e 's,^..,||,g; /[^-]$/s/$/ |/' > ./temp2.txt)
+$(cat ./temp2.txt | sed -e 's,^\(.\{73\}\),\1 ||,' > ./temp3.txt)
+$(cat ./temp3.txt | sed -e '/^\(.\{0,9\}\)[A-Z]\{3,\}/s/$/|/;' > keymapPrint.txt )
 
-cat "$_temp" | sed 's/^../||/g; s/\(.\{73\}\)/\1||/g; s/\(.\{75\}\).*/\1/g  > ./keymapPrint.txt
-
+cat $_temp
+cat keymapPrint.txt
+lp -d cups-PDF keymapPrint.txt
 
 
 
